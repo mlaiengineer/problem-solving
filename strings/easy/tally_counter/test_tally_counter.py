29c9198410969f44ebd95f39985b9be0b6e16a56
Abdulbaset Saleh <abdulbaset0569@gmail.com>
2026-07-13 21:40:45 +0300

    Add test_get_tally_count function

---
+++ strings/easy/tally_counter/test_tally_counter.py
@@ -0,0 +1,6 @@
+from tally_counter import get_tally_count
+
+#test cases
+print(get_tally_count("||||"))
+print(get_tally_count("||||/ |||"))
+print(get_tally_count("||||/ ||||/ ||||/ ||||/ ||||/ ||||/ ||||/ ||||/ |"))
